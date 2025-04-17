0d1cda935bd8cab208499ab91981f07e813c89bb
Jesse Talavera <jesse@jesse.tg>
2025-04-17 17:25:36 -0400

    Get the time since Steam was started

--- main.py
+++ main.py
@@ -22,6 +22,72 @@ class Times(TypedDict):
     last_wake_time: int
     plugin_start_time: int
 
+
+def get_steam_start_time():
+    try:
+        # Open "~/.steampid"
+        steam_pid_path = os.path.expanduser("~/.steampid")
+        # Open "/home/deck/.steam/steam.pid" if the first one doesn't exist
+        if not os.path.exists(steam_pid_path):
+            steam_pid_path = "/home/deck/.steam/steam.pid"
+
+        if not os.path.exists(steam_pid_path):
+            logger.warning(f"Steam PID file not found at {steam_pid_path}")
+            return 0
+
+        # Read the file to get the PID of Steam
+        with open(steam_pid_path, 'r') as f:
+            steam_pid = int(f.read().strip())
+
+        # Ensure that the PID still exists
+        if not os.path.exists(f"/proc/{steam_pid}"):
+            logger.warning(f"Steam process with PID {steam_pid} not found")
+            return 0
+
+        # Read /proc/<pid>/stat
+        with open(f"/proc/{steam_pid}/stat", 'r') as f:
+            stat_data = f.read().strip()
+
+        # Parse stat data (handling command with spaces in parentheses)
+        stat_parts = stat_data.split(')')
+        if len(stat_parts) < 2:
+            logger.warning(f"Invalid stat data format for PID {steam_pid}")
+            return 0
+
+        stat_values = stat_parts[1].strip().split()
+
+        # Read the "starttime" field (#22, 1-indexed, which is index 21 in 0-indexed array)
+        # But due to our special parsing for the command name, we need to adjust
+        start_time_ticks = int(stat_values[19])  # 22 - 2 = 20, then 0-indexed = 19
+
+        # Get clock ticks per second
+        clock_ticks = os.sysconf(os.sysconf_names['SC_CLK_TCK'])
+
+        if clock_ticks <= 0:
+            logger.warning("Invalid clock ticks value: {}".format(clock_ticks))
+            return 0
+
+        # Get system boot time
+        with open("/proc/stat", 'r') as f:
+            for line in f:
+                if line.startswith("btime "):
+                    boot_time_secs = int(line.split()[1])
+                    break
+            else:
+                logger.warning("Could not find boot time in /proc/stat")
+                return 0
+
+        # Convert the "starttime" field from clock ticks to milliseconds since epoch
+        start_time_secs = boot_time_secs + (start_time_ticks / clock_ticks)
+        start_time_ms = int(start_time_secs * 1000)
+
+        return start_time_ms
+
+    except Exception as e:
+        logger.error(f"Error getting Steam start time: {e}")
+        return 0
+
+
 class Plugin:
     def __init__(self):
         logger.debug("Initializing Sincere Clock")
@@ -38,15 +104,6 @@ class Plugin:
         # TODO: Get the time that the current game started
         return None
 
-    def _get_steam_start_time(self):
-        # TODO: Get the time that Steam started
-        # TODO: Open "~/.steampid"
-        # TODO: Open "/home/deck/.steam/steam.pid" if the first one doesn't exist
-        # TODO: Read the file to get the PID of Steam
-        # TODO: Ensure that the PID still exists
-        # TODO: Read /proc/<pid>/stat to get the start time
-        return 0
-
     def _get_last_wake_time(self):
         # TODO: Figure out how to tell when the Deck was last woken up
         return 0
@@ -57,7 +114,7 @@ class Plugin:
             plugin_start_time=int(self._plugin_start_time.timestamp() * 1000),
             boot_time=self._get_boot_time(),
             game_start_time=self._get_game_start_time(),
-            steam_start_time=self._get_steam_start_time(),
+            steam_start_time=get_steam_start_time(),
             last_wake_time=self._get_last_wake_time(),
         )
 
