12dfe51c2ba43ed8e07f75382b2bd8f4552a7134
Jesse Talavera <jesse@jesse.tg>
2025-04-24 20:45:58 -0400

    Fix `eslint.config.js`

--- eslint.config.js
+++ eslint.config.js
@@ -5,11 +5,27 @@ import pluginReact from "eslint-plugin-react";
 import pluginReactHooks from "eslint-plugin-react-hooks";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-  pluginReact.configs.recommended,
-  pluginReactHooks.configs.recommended,
+  {
+    files: ["src/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    plugins: { js },
+    extends: [js.configs.recommended],
+  },
+  {
+    files: ["src/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: { globals: globals.browser }
+  },
+  ...tseslint.configs.recommended,
+  {
+    ...pluginReact.configs.flat.recommended,
+    plugins: {
+      react: pluginReact,
+    },
+  },
+  {
+    ...pluginReactHooks.configs.recommended,
+    plugins: {
+      "react-hooks": pluginReactHooks,
+    },
+  },
 ]);
