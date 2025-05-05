8809a2a7d7a61574364fd1a220e774319685a959
Jesse Talavera <jesse@jesse.tg>
2025-05-05 18:59:36 -0400

    Implement `<TimeSinceGameStarted/>`

---
+++ src/components/clock/TimeSinceGameStarted.tsx
@@ -0,0 +1,26 @@
+import {Temporal} from "temporal-polyfill";
+import {format} from "../../utils/timeUtils";
+import {ReactElement} from "react";
+import {LuGamepad2} from "react-icons/lu";
+
+export interface TimeSinceGameStartedProps {
+    startTime: Temporal.Instant | null;
+    now: Temporal.Instant;
+}
+
+export default function TimeSinceGameStarted(props: TimeSinceGameStartedProps) {
+    let component: ReactElement;
+    if (props.startTime) {
+        component = <span className={"duration"}>{format(props.now.since(props.startTime))}</span>;
+    }
+    else {
+        component = <span className={"duration"}>-:-:-</span>;
+    }
+
+    return (
+        <div className={"clock"}>
+            <LuGamepad2 className={"icon"}/>
+            {component}
+        </div>
+    );
+}
